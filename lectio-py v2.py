a4286ae4d5a726b9679851ffadf65c8531d18887
KongKret <34191076+keti0064@users.noreply.github.com>
2023-06-27 17:08:10 +0200

    Update lectio-py v2.py
    lavet videre på besked modul

--- lectio-py v2.py
+++ lectio-py v2.py
@@ -70,19 +70,43 @@ def test(skoleID,session,printToggle=False):
 # liste med alle overskrifter tid og start person og link til videre data
 def get_all_messages(skoleID,elevID,session):
     besked_url = "https://www.lectio.dk/lectio/{0}/beskeder2.aspx?type=&elevid={1}&selectedfolderid".format(skoleID,elevID)
-
+    beskeder = []
     soup = getSoup(besked_url,session)
-    print(soup.findAll("tr"))
+    tr_list = soup.findAll("tr")
+    for tr in tr_list[1:]:
+        td_list = tr.findAll("td")
 
 
+        td_list_text = [x.text for x in td_list]
+
+
+        ID_tag = (tr.findNext("a",tabindex="0")["onclick"])
+        ID = ID_tag[ID_tag.index("MC_$_")+5:ID_tag.index(")")]
+
+
+        besked = Besked(td_list_text[3].replace("\n",""),td_list_text[4],td_list_text[6],td_list_text[5],td_list_text[8],ID)
+
+        beskeder.append(besked)
+
+    return beskeder
 
 class Besked:
-    def __init__(self, titel, sender, modtager, dato, link):
+    def __init__(self, titel, sender, modtager,seneste_besked, dato, ID):
         self.titel = titel
         self.sender = sender
         self.modtager = modtager
+        self.seneste_besked = seneste_besked
         self.dato = dato
-        self.link = link
+        self.ID = ID
+
+    def consolePrintMessageInfo(self):
+        print(self.titel,self.sender,self.modtager,self.seneste_besked,self.dato,self.ID)
+
+    def getMessageDialog(self,skoleID,elevID,session):
+        besked_url = "https://www.lectio.dk/lectio/{0}/beskeder2.aspx?type=showthread&elevid={1}&selectedfolderid=-70&id={2}".format(skoleID,elevID,self.ID)
+
+        pageSoup = getSoup(besked_url,session)
+        print(pageSoup.prettify())
 
 
 
@@ -90,7 +114,8 @@ class Besked:
 sesh = getLoginSession("","","523")
 e_id= get_elev_ID("523",sesh)
 
-get_all_messages("523",e_id,sesh)
+a = get_all_messages("523",e_id,sesh)
+a[1].getMessageDialog("523",e_id,sesh)
 
 
 """

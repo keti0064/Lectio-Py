10870c605c5ef0fdda8354c717d72e840db41246
KongKret <34191076+keti0064@users.noreply.github.com>
2023-11-05 15:20:14 +0100

    Update lectio-py v2.py
    opretter modul objekter

--- lectio-py v2.py
+++ lectio-py v2.py
@@ -1,8 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-
 
 
 def getLoginSession(username,password,schoolID):
@@ -28,11 +25,15 @@ def getLoginSession(username,password,schoolID):
     return loginSession
 
 # retunere suppen til en URL med den active session(altså det aktive login :)
+
+
 def getSoup(URL, session):
     return BeautifulSoup(session.get(URL).content,"html.parser")
 
+
 def postSoup(URL,session,payload):
     return BeautifulSoup(session.post(URL,data=payload).content,"html.parser")
+
 
 def get_elev_ID(skoleID,session):
     # går ind på forsiden (den kræver ikke elevid i URL men kun cookie) elevID findes i en specifik a-tag da den linker
@@ -56,7 +57,6 @@ def get_elev_ID(skoleID,session):
 
 def test(skoleID,session,printToggle=False):
     soup= getSoup("https://www.lectio.dk/lectio/{0}/forside.aspx".format(skoleID),session)
-
     data = soup.find("div", id="s_m_HeaderContent_MainTitle").text
 
     if printToggle == True:
@@ -73,23 +73,21 @@ def get_all_messages(skoleID,elevID,session):
     beskeder = []
     soup = getSoup(besked_url,session)
     tr_list = soup.findAll("tr")
+
     for tr in tr_list[1:]:
         td_list = tr.findAll("td")
-
-
         td_list_text = [x.text for x in td_list]
-
 
         ID_tag = (tr.findNext("a",tabindex="0")["onclick"])
         # skal minus med 1 for at ikke få et ' med
         ID = ID_tag[ID_tag.index("MC_$_")+5:ID_tag.index(")")-1]
-
 
         besked = Besked(td_list_text[3].replace("\n",""),td_list_text[4],td_list_text[6],td_list_text[5],td_list_text[8],ID)
 
         beskeder.append(besked)
 
     return beskeder
+
 
 class Besked:
     def __init__(self, titel, sender, modtager,seneste_besked, dato, ID):
@@ -103,7 +101,6 @@ class Besked:
     def consolePrintMessageInfo(self):
         print(self.titel,self.sender,self.modtager,self.seneste_besked,self.dato,self.ID)
 
-    # det her er besværligt da det kræver viewstatex i en post request for at få den her side som bliver lavet om hver gang man går ind på besked forsiden selv i samme session :(
     def getMessageDialog(self,skoleID,elevID,session):
         besked_url = "https://www.lectio.dk/lectio/{0}/beskeder2.aspx?type=showthread&elevid={1}&selectedfolderid=-70&id={2}".format(skoleID,elevID,self.ID)
         besked_forside_url = "https://www.lectio.dk/lectio/{0}/beskeder2.aspx?type=&elevid={1}&selectedfolderid".format(skoleID, elevID)
@@ -121,7 +118,6 @@ class Besked:
             "LectioPostbackId": ""
         }
 
-
         pageSoup = postSoup(besked_url,session,postPayload)
         # sorter efter svar
         svar = pageSoup.find("ul",id="s_m_Content_Content_ThreadList").findAll("li")
@@ -138,8 +134,7 @@ class Besked:
             print(tab,"[x]",sender,"\n",tab,text,"\n")
 
 
-# anskaf alle moduler i DENNE uge.
-
+# anskaf alle moduler i en uge
 def get_all_moduler(schoolID,elevID,session,week_year="X",):
     if week_year == "X":
 
@@ -149,47 +144,66 @@ def get_all_moduler(schoolID,elevID,session,week_year="X",):
     siteSoup = getSoup(siteUrl, session)
 
     # find alle moduler, de kan blive placeret ud fra dato som står på modulet.
-    days = siteSoup.findAll("div", {"class":"s2skemabrikcontainer lec-context-menu-instance"})
-    moduler = []
-    for day in days:
+    alleDageDivs = siteSoup.findAll("div", {"class":"s2skemabrikcontainer lec-context-menu-instance"})
+
+    #liste med alle modul html elementerne på en uge
+    ugeModuler = []
+
+    # iterere igennem alle dage divs, finder alle a_tags, som er hvert enkelt modul og ligger det i en liste
+    # den liste bliver smidt ind i moduler, så hver dag er en liste af moduler i moduler
+    for dagDiv in alleDageDivs:
         # generer modul objekter
-        moduler.append(day.findAll("a"))
-    for dag,modul in enumerate(moduler):
-        for a_num, a_tag in enumerate(modul):
+        ugeModuler.append(dagDiv.findAll("a"))
 
-
-
+    # liste af dage med moduler omskrevet til modul objekter
+    denneUge = []
+    for dagNum,dag in enumerate(ugeModuler):
+        # liste af denne dags moduler, som modul objektet
+        denneDag=[]
+        for a_num, a_tag in enumerate(dag):
             data = a_tag.find("div",{"class":"s2skemabrikcontent"})
-            #print(modul)
             if data != None:
 
-                print("////////////////////////\nNYT MODUL\n")
+                # nyt modul
                 modul_titel = ""
-                a_tag_href= a_tag["href"]
+
+                # Der tages højde for om modulet ikke har et ID
+                try:
+                    # modulets ID ligger i modulets link i absid attributten
+                    a_tag_href= a_tag["href"]
+                    modul_ID = a_tag_href[a_tag_href.index("absid=") + 6:a_tag_href.index("&")]
+
+                except:
+                    modul_ID = ""
+
                 a_tag_style = a_tag["style"]
 
                 # hvert modul har et tool tip hvor lokale, note og tidspunkt står i.
                 a_tag_additional_info = a_tag["data-additionalinfo"]
-                Lokale_Index = a_tag_additional_info.index("Lokale:")+8
-                modul_lokale = a_tag_additional_info[Lokale_Index:a_tag_additional_info.find("\n",Lokale_Index)]
 
+
+                Lokale_Index = a_tag_additional_info.find("Lokale:")
+                if Lokale_Index != -1:
+                    modul_lokale = a_tag_additional_info[Lokale_Index:a_tag_additional_info.find("\n",Lokale_Index+8)]
+                else:
+                    modul_lokale = ""
                 # Modul tid og dato, findes også i additional info, den ligger altid over "hold" og alle moduler
                 # har hold og dato/tid så det vil altid virke, ellers skriver den FEJL og så ved jeg ikke lige
                 # hvad der skal gøres.
                 additional_info_splitt = a_tag_additional_info.split("\n")
+
+                # hedder fejl til hvis der ikke findes nogen valid værdi
                 modul_dato_tid = "FEJL"
                 for index, newLine in enumerate( additional_info_splitt):
                     if "Hold:" in newLine:
                         modul_dato_tid = additional_info_splitt[index-1].replace("\n","")
-
-                # modulets ID ligger i modulets link i absid attributten
-                modul_ID= a_tag_href[a_tag_href.index("absid=")+6:a_tag_href.index("&")]
 
                 # den fysiske position på dagens skema ligger i modulets stil, samt størrelsen af modulet
                 modul_pos = a_tag_style
                 # modul data
                 modul_lære = []
                 modul_hold = []
+                modul_titel = ""
 
                 modul_status = a_tag.attrs["class"][2]
                 all_span = data.findAll("span")
@@ -208,56 +222,47 @@ def get_all_moduler(schoolID,elevID,session,week_year="X",):
                         # her findes titlen til modulet, da span tagget med titlen ikke har en contextcard attr
                         # som den eneste.
                         modul_titel = span.text
+                #print("\n"+str(dagNum)+"\n"+modul_titel+"\n"+modul_dato_tid+"\n"+modul_lokale+"\n"+modul_status+"\n"+" ".join(modul_hold)+"\n"+" ".join(modul_lære)+"\n"+modul_ID+"\n"+modul_pos+"\n")
 
-                print(str(dag)+"\n"+modul_titel+"\n"+modul_dato_tid+"\n"+modul_lokale+"\n"+modul_status+"\n"+" ".join(modul_hold)+"\n"+" ".join(modul_lære)+"\n"+modul_ID+"\n"+modul_pos)
-
-                print("\nFÆRDIGT MODUL\n////////////////////////\n")
+                # lav modul objekt og tilføj til liste
+                denneDag.append(Modul(modul_lære,modul_hold,modul_pos,modul_lokale,modul_dato_tid,modul_status,modul_ID,modul_titel))
+                #slut modul
             else:
                 continue
+
+        denneUge.append(denneDag)
+    return denneUge
+
 
 class Modul:
     #anskaffer modulerne ude fra, men de bliver lavet til objekter her. som tillader nem refferat og nemt tilgængeligt
     #data fra hvert modul og data fra modulets side
 
-    def __init__(self, lærer,hold, position,lokale,tid,note,lektie,eksta_indhold,status,id):
+    def __init__(self, lærer,hold, position,lokale,tid,status,id,titel):
         self.lærer = lærer
         self. hold = hold
         self.position = position
         self.lokale = lokale
         self.tid = tid
-        self.note = note
-        self.lektie = lektie
-        self.ekstra_indhold = eksta_indhold
         self.status = status
         self.id = id
+        self.titel = titel
 
-    def get_site_data(self,skoleID,elevID):
-        #Her behøves der ikke at anskaffes viewstatex, get requesten kan bare køres direkte
+    def get_site_data(self, skoleID, elevID, session):
+        # Her behøves der ikke at anskaffes viewstatex, get requesten kan bare køres direkte
+        siteURL = "https://www.lectio.dk/lectio/{0}/aktivitet/aktivitetforside2.aspx?absid={1}&prevurl=SkemaNy.aspx%3ftype%3delev%26elevid%3d{2}&elevid={2}".format(skoleID,self.id,elevID)
+        pageSoup = getSoup(siteURL,session)
 
-        siteURL = "/lectio/{0}/aktivitet/aktivitetforside2.aspx?absid={1}&prevurl=SkemaNy.aspx%3ftype%3delev%26elevid%3d{2}&elevid={2}".format(skoleID,self.id,elevID)
+        # find note hvis der er nogen
+        try:
+            note = pageSoup.find("textarea",attrs={"class":"activity-note"}).text
+        except:
+            note = None
 
-        pageSoup = getSoup(siteURL)
+        # find lektier og eller øvrigt hvis der er nogen
+        try:
+            lektier_øvrigt = pageSoup.find("div",id="s_m_Content_Content_tocAndToolbar_inlineHomeworkDiv").text
+        except:
+            lektier_øvrigt = None
 
-
-        #midlertidlig test
-        print(pageSoup.text)
-
-
-
-#test
-sesh = getLoginSession("","","523")
-e_id= get_elev_ID("523",sesh)
-
-
-get_all_moduler("523",e_id,sesh,"342023")
-
-
-"""
-# grafiske interface
-class app(MDApp):
-    def build(self):
-        label = MDLabel(text="Dit Lectio", halign='center',theme_text_color='Error')
-        return label
-
-#app().run()
-"""
+        return note, lektier_øvrigt
